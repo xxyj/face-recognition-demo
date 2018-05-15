8ee98d97d32f9bf33692b790eddbd98f39ce0335
hzxiongxu <xx@xiongxudeMacBook-Pro.local>
2018-05-15 20:15:11 +0800

    fix

--- demo/chrome/read.js
+++
@@ -1,6 +0,0 @@
-window.onmouseup = function() {
-  var selection = window.getSelection();
-  if (selection.anchorOffset != selection.extentOffset) {
-    chrome.runtime.sendMessage(selection.toString());
-  }
-};
